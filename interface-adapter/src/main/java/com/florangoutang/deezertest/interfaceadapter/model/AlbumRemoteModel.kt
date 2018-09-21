a7e21b80a5299c5e9e836019c98e5bd45937e71d
florent_dambreville <fdambreville.pres>
2018-09-21 17:11:13 +0200

    Display list !

--- interface-adapter/src/main/java/com/florangoutang/deezertest/interfaceadapter/model/AlbumRemoteModel.kt
+++ interface-adapter/src/main/java/com/florangoutang/deezertest/interfaceadapter/model/AlbumRemoteModel.kt
@@ -1,8 +1,12 @@
 package com.florangoutang.deezertest.interfaceadapter.model
 
+import com.google.gson.annotations.SerializedName
+
 data class AlbumRemoteModel(val data : List<AlbumData?>) {
 
-    data class AlbumData(val id: Int?, val title: String?, val coverUrl: String?, val artist: AlbumArtist?) {
+    data class AlbumData(val id: Int?,
+                         val title: String?,
+                         @SerializedName("cover_medium") val coverUrl: String?, val artist: AlbumArtist?) {
 
         data class AlbumArtist(val id: Int?, val name: String?, val pictureUrl: String?)
     }
